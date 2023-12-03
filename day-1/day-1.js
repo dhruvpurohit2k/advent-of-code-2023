728450b935f97c6f819045d4270de10fc55d8a94
Dhruv Purohit <purohitdhruv00@gmail.com>
2023-12-03 12:16:48 +0530

    Learnt about RegExp and match. Used to get the answer.

--- day-1/day-1.js
+++ day-1/day-1.js
@@ -1,5 +1,20 @@
 const fs = require("fs");
-fs.readFile("input.txt", (err, inputD) => {
-  if (err) throw err;
-  console.log(inputD.toString());
-});
+
+fs.readFile(
+  "/home/dhruv/coding/js/advent-of-code-2023/day-1/input.txt",
+  (err, inputD) => {
+    if (err) throw err;
+    let file = inputD.toString();
+    let stringArray = new Array();
+    stringArray = file.split("\n");
+    stringArray.pop();
+    let sum = 0;
+    stringArray.forEach((string, index, array) => {
+      let re = /\d/g;
+      let nums = string.match(re);
+      let final = new Number(nums[0] + nums[nums.length - 1]);
+      sum += final;
+    });
+    console.log(sum);
+  },
+);
